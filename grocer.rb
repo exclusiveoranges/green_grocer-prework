09a39ec8cc08490e5c73261e94082a48997143b5
Ryan O'Toole <otoolerj@gmail.com>
2018-03-29 23:38:01 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -55,7 +55,7 @@ def checkout(cart, coupons)
   new_cart_clearance = apply_clearance(new_cart_coupon)
   total_price = 0
   new_cart_clearance.each do |hash|
-      
+      binding.pry
     hash.each do |key, value|
       total_price += hash[key][:price] * hash[key][:count]
     end
